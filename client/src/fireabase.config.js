e3dfeb31fa7abb1bee18bdb12295989ee3f38807
Your Name <rajkumarsahu2002@gmail.com>
2023-06-14 20:46:24 +0530

    first commit

---
+++ client/src/fireabase.config.js
@@ -0,0 +1,45 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAMbiiquFsXyEc7h_gfNLB2Jo-p993kcvg",
+//   authDomain: "bazar-d3cb2.firebaseapp.com",
+//   projectId: "bazar-d3cb2",
+//   storageBucket: "bazar-d3cb2.appspot.com",
+//   messagingSenderId: "290833778260",
+//   appId: "1:290833778260:web:56aa636d4d6a3a38e48fc1",
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA7IhnwaCY5_Jc7suzvO-JhWZG9uBJW2EE",
+  authDomain: "bazar-bcbfc.firebaseapp.com",
+  databaseURL: "https://bazar-bcbfc-default-rtdb.firebaseio.com",
+  projectId: "bazar-bcbfc",
+  storageBucket: "bazar-bcbfc.appspot.com",
+  messagingSenderId: "582806760261",
+  appId: "1:582806760261:web:28d92eb188a0fc71a3ac2c",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+// app.initializeApp(firebaseConfig);
+
+// const serviceAccount = require("C:UsersDellDownloads\bazar-master\bazar-masterclientsrc\bazar-bcbfc-firebase-adminsdk-sg1yi-046c1c1470.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://bazar-bcbfc-default-rtdb.firebaseio.com/",
+// });
